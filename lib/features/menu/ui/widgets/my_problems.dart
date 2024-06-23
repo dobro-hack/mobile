300832615d242d953f05d69ebffc3ab715f29d76
MariyaVik <pmarvik21@gmail.com>
2024-06-23 13:59:15 +0500

    show more my problem

--- lib/features/menu/ui/widgets/my_problems.dart
+++ lib/features/menu/ui/widgets/my_problems.dart
@@ -2,6 +2,7 @@ import 'package:eco/features/problem/data/models/problem_back.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../common/theme/app_colors.dart';
 import '../../../problem/data/models/problem_data.dart';
 import '../../../problem/data/models/problem_local.dart';
 import '../../../problem/domain/problem_provider.dart';
@@ -17,6 +18,8 @@ class MyProblems extends ConsumerStatefulWidget {
 }
 
 class _MyProblemsState extends ConsumerState<MyProblems> {
+  bool showAll = false;
+
   @override
   void initState() {
     super.initState();
@@ -42,24 +45,53 @@ class _MyProblemsState extends ConsumerState<MyProblems> {
           ),
         ),
         asyncProblems.when(
-            loading: () => const Center(
-                  child: CircularProgressIndicator(),
-                ),
-            error: (Object error, StackTrace stackTrace) =>
-                Text(error.toString()),
-            data: (List<ProblemData> data) {
-              return data.isEmpty
-                  ? const NoProblemsBlock()
-                  : Column(
-                      children: data
-                          .map(
-                            (e) => e is ProblemBack
-                                ? SendedProblem(problemBack: e)
-                                : NoSendedProblem(problem: e as LocalProblem),
-                          )
-                          .toList(),
-                    );
-            }),
+          loading: () => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          error: (Object error, StackTrace stackTrace) =>
+              Text(error.toString()),
+          data: (List<ProblemData> data) {
+            return data.isEmpty
+                ? const NoProblemsBlock()
+                : Column(
+                    children: [
+                      ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: showAll
+                            ? data.length
+                            : (data.length > 3 ? 3 : data.length),
+                        itemBuilder: (contex, index) {
+                          return data[index] is ProblemBack
+                              ? SendedProblem(
+                                  problemBack: data[index] as ProblemBack)
+                              : NoSendedProblem(
+                                  problem: data[index] as LocalProblem);
+                        },
+                      ),
+                      if (data.length > 3)
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: AppColors.greyBackgroundLight,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  showAll = !showAll;
+                                });
+                              },
+                              child:
+                                  Text(showAll ? 'Свернуть' : 'Показать ещё'),
+                            ),
+                          ),
+                        ),
+                    ],
+                  );
+          },
+        ),
       ],
     );
   }
